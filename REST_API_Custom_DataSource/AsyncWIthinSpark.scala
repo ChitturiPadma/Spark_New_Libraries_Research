dc36c4a15745d4b92fc06e4db3bb380b81a8a3c9
Padma Chitturi <padmapriya30@gmail.com>
2018-07-08 17:04:11 +0530

    Update AsyncWIthinSpark.scala

--- REST_API_Custom_DataSource/AsyncWIthinSpark.scala
+++ REST_API_Custom_DataSource/AsyncWIthinSpark.scala
@@ -1,8 +1,5 @@
 package com.spark.demos
-
-
 import org.apache.spark._
-
 import scala.util.{Success, _}
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -14,22 +11,15 @@ object ConcurrentContext {
   import scala.concurrent.duration.Duration._
 
   def executeAsync[T](f: => T):Future[T] = Future(f)
-
   def futureToFutureOption[T](f: Future[T]): Future[Option[T]] =
     f.map(Some(_)).recover {
       case e => None
     }
-
   def awaitAll(iter:Iterator[Future[(Int,Try[Int])]], timeout:Duration = 3.second) = {
-
     Await.result(Future.sequence(iter), timeout)
   }
-
   def awaitBatch[T](iter:Iterator[Future[(Int,Try[Int])]], batchSize:Int =  10, timeout:Duration = 5.second) ={
-
     val grouped = iter.grouped(batchSize).map{x => Future.sequence(x)}
-
-
     iter.grouped(batchSize)
       .map(batch => Future.sequence(batch))
       .flatMap{futureBatch =>  Await.result(futureBatch, timeout).toIterator
@@ -37,15 +27,13 @@ object ConcurrentContext {
   }
 }
 object AsyncWithinSpark {
-
-
+  
   def slowFunc(x:Int):(Int, Try[Int])={
     println(s"slowFunc start ($x)")
     val res =  Try(if(x.isInstanceOf[Int] && x%10 == 0) (x.asInstanceOf[Int]/0).asInstanceOf[Int] else x.asInstanceOf[Int])
     println(s"slowFunc end($x)")
     (x,res)
   }
-
   def fastFunc[T](x:T):T = {
     println(s"fastFunc($x)")
     x
@@ -53,14 +41,11 @@ object AsyncWithinSpark {
 
   def main(args:Array[String]): Unit ={
 
-    val conf = new SparkConf().setMaster("local[2]").setAppName("ConcurrencySpark_Demo").set("spark.driver.host","localhost")
+    val conf = new SparkConf().setMaster("spark://<ip>:7077").setAppName("ConcurrencySpark_Demo").set("spark.driver.host","localhost")
     val sc = new SparkContext(conf)
-
     val intRdd = sc.parallelize(1 to 20, 2)
-
     println("Async Results")
     val asyncRes = intRdd.map{ele => fastFunc(ele)}.map{ele =>ConcurrentContext.executeAsync(slowFunc(ele)) }
-
       .mapPartitions{
       it =>
        /* Unbounded concurrent execution using Futures  */
@@ -75,7 +60,6 @@ object AsyncWithinSpark {
       }
     }
     asyncRes.collect().foreach(println)
-
   }
 
 }
